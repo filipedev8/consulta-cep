038e5a991bf6849b5ce3dbf1cc75c8997beade3a
filipedev8 <eulerfilipe8@gmail.com>
2022-06-30 23:20:42 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,22 @@
+<p align="center">
+ <img src="./images/project"/>
+</p>
+
+<br>
+
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML
+- CSS
+- JavaScript
+
+Bibliotecas
+
+- [Google Fonts](https://fonts.google.com/)
+
+Projeto
+
+- Projeto Desenvolvido para consulta de CEP em todas as regiões do Brasil teste você agora mesmo :)
---
+++ main.js
@@ -0,0 +1,37 @@
+const cep = document.getElementById('cep');
+const buscarCep = document.getElementById('btnBuscar'); 
+const btnDarkLight = document.querySelector('.btn-darkLight')
+
+buscarCep.addEventListener('click', () => {
+    const logradouro = document.getElementById('logradouro');
+    const complemento =  document.getElementById('complemento');
+    const bairro = document.getElementById('bairro');
+    const localidade = document.getElementById('localidade');
+    const estado = document.getElementById('estado');
+
+    if(cep.value === "") {
+        return alert('Precha o Campo "Digite aqui seu CEP" e tente novamente :)')
+    } else if(cep.value.length < 8) {
+        return alert('Formato de CEP inválido.')
+    }
+
+    
+
+    const request = axios.get(`https://viacep.com.br/ws/${cep.value}/json/`).then(({data}) => {
+        if(data?.erro){
+            return alert('O CEP Informado Não foi encontrado')  
+        } else{
+            logradouro.value = data.logradouro
+            complemento.value = data.complemento
+            bairro.value = data.bairro
+            localidade.value = data.localidade
+            estado.value = data.uf
+        }
+        document.getElementById('flex').style.display = "flex";
+    })
+});
+
+btnDarkLight.addEventListener('click', function() {
+    this.classList.toggle('dark-light')
+    document.querySelector('body').classList.toggle('dark-light')
+})
